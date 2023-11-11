0635b09dc1d08f36e6b50ab8180b90f8adf5643a
Robin Saini <robinsaini2126@gmail.com>
2023-11-11 21:40:02 +0530

    added pre-commit-script

---
+++ pre-commit-script.js
@@ -0,0 +1,31 @@
+const fs = require("node:fs");
+
+(function () {
+  const packageInfo = JSON.parse(fs.readFileSync("./package.json").toString() ?? {});
+  const packageVersion = packageInfo?.version;
+  console.log(packageVersion)
+  let [first, second, third] = packageVersion
+    .split(".")
+    .map((verInfo) => parseInt(verInfo));
+  third += 1;
+  if (third > 9) {
+    third = 0;
+    second += 1;
+  }
+
+  if (second > 9) {
+    second = 0;
+    first += 1;
+  }
+
+  if (second > 9 && third > 9) {
+    third = 0;
+    second = 1;
+    first += 1;
+  }
+
+  packageInfo.version =`${first}.${second}.${third}`
+  console.log(packageInfo)
+  fs.writeFileSync("./package.json",JSON.stringify(packageInfo ?? {}))
+  //console.log(JSON.parse(packageInfo.toString() ?? {}))
+})();
